b510ac5b7986730da7d966d3c2add1230ef90b90
Erik Lance <73865149+erik-lance@users.noreply.github.com>
2024-04-12 23:13:50 +0800

    Added logo to all pages

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,6 +5,8 @@ import ThemeWrapper from "@/components/ThemeWrapper";
 import { AppProps } from "next/app";
 import { ThemeProvider } from "@emotion/react";
 import theme from "@/styles/theme";
+import Image from "next/image";
+import { Container } from "@mui/material";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -12,6 +14,24 @@ function MyApp({Component, pageProps}: AppProps) {
     return (
         <ThemeProvider theme={theme}>
             <Component {...pageProps} />
+
+        {/* Logo at bottom left corner */}
+        <Container
+            sx={{
+                position: "fixed",
+                bottom: 0,
+                left: 0,
+                padding: 1,
+            }}
+        >
+            <Image
+            src="/cataclysm_logo_yellow_small.png"
+            alt="Logo"
+            width={25}
+            height={25}
+            />
+        </Container>
+        
         </ThemeProvider>
     )
 }
